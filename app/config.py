426c1c4b3ceeb31c9ef3f91f29211e46571b1099
roschel <joaomroschel@gmail.com>
2022-10-05 22:52:17 -0300

    updating nothing

--- app/config.py
+++ app/config.py
@@ -7,18 +7,18 @@ from pydantic import BaseSettings, Field
 class Settings(BaseSettings):
 
     # API LOTERIAS
-    API_LOTERIAS_URL: str
+    API_LOTERIAS_URL: str = Field("https://loteriascaixa-api.herokuapp.com/api")
 
     # POSTGRES
-    DB_USERNAME: str = Field("", title="Usuário do banco de dados")
-    DB_PASSWORD: str = Field("", title="Senha")
-    DB_HOST: str = Field("", title="Host")
-    DB_PORT: str = Field("", title="Porta")
+    DB_USERNAME: str = Field("postgres", title="Usuário do banco de dados")
+    DB_PASSWORD: str = Field("password", title="Senha")
+    DB_HOST: str = Field("localhost", title="Host")
+    DB_PORT: str = Field("5432", title="Porta")
     DB_DATABASE: str = Field("", title="Banco de dados")
-    REPOSITORY_NAME: str
+    REPOSITORY_NAME: str = Field("postgresql")
 
     class Config:
-        env_file = os.getenv('ENV_FILE')
+        env_file = os.getenv("ENV_FILE")
 
 
 @lru_cache
